0812c275a5f3fd67006c80b5190ebdd1da09af3d
ksadov <ksadov@gmail.com>
2025-04-05 11:37:28 -0500

    get rid of ragatouille

--- chat/retrieval.py
+++ chat/retrieval.py
@@ -40,21 +40,21 @@ def load_parquet_documents(document_path, alias_dict, include_timestamp, name):
     metadatas = [make_metadata_dict(entry) for entry in entries]
     # check if alias_field is in metadata
     for i, metadata in enumerate(metadatas):
-        if metadata['user_id'] is None:
-            metadata['user_id'] = name
-        if metadata['user_id'] in alias_dict:
-            metadata['user_id'] = alias_dict[str(metadata['user_id'])]
+        if metadata["user_id"] is None:
+            metadata["user_id"] = name
+        if metadata["user_id"] in alias_dict:
+            metadata["user_id"] = alias_dict[str(metadata["user_id"])]
         if include_timestamp:
-            timestamp = pd.to_datetime(
-                metadata['timestamp'])
+            timestamp = pd.to_datetime(metadata["timestamp"])
             # if year is equal to 1970, convert from unix timestamp
             if timestamp.year == 1970:
-                timestamp = pd.to_datetime(
-                    metadata['timestamp'], unit='ms').strftime('%Y-%m-%d %H:%M')
+                timestamp = pd.to_datetime(metadata["timestamp"], unit="ms").strftime(
+                    "%Y-%m-%d %H:%M"
+                )
             elif timestamp.year < 2000:
                 timestamp = f"Unknown Timestamp"
             else:
-                timestamp = timestamp.strftime('%Y-%m-%d %H:%M')
+                timestamp = timestamp.strftime("%Y-%m-%d %H:%M")
             timestamp_prefix = f"[{timestamp}]"
         else:
             timestamp_prefix = ""
@@ -66,18 +66,17 @@ def prep_parquet_documents(document_path, alias_dict, include_timestamp, name):
     # check if there are seperate /chat and /blog subdirectories
     if os.path.exists(os.path.join(document_path, "blog")):
         chat_documents, chat_metadatas = load_parquet_documents(
-            os.path.join(
-                document_path, "chat"), alias_dict, include_timestamp, name
+            os.path.join(document_path, "chat"), alias_dict, include_timestamp, name
         )
     if os.path.exists(os.path.join(document_path, "blog")):
         blog_documents, blog_metadatas = load_parquet_documents(
-            os.path.join(
-                document_path, "blog"), alias_dict, include_timestamp, name
+            os.path.join(document_path, "blog"), alias_dict, include_timestamp, name
         )
     else:
         # assuming all are chat documents
         chat_documents, chat_metadatas = load_parquet_documents(
-            document_path, alias_dict, include_timestamp)
+            document_path, alias_dict, include_timestamp
+        )
         blog_documents, blog_metadatas = [], []
     return chat_documents, chat_metadatas, blog_documents, blog_metadatas
 
@@ -87,17 +86,21 @@ def filter_and_chunk(documents, metadatas, chunk_depth, name=None, overlap=0):
     filtered_documents = []
     filtered_metadatas = []
     # partition documents by conversation
-    unique_conversations = list(set([metadata["conversation"]
-                                     for metadata in metadatas]))
+    unique_conversations = list(
+        set([metadata["conversation"] for metadata in metadatas])
+    )
     for conversation in tqdm(unique_conversations):
-        conv_ids = [i for i, metadata in enumerate(
-            metadatas) if metadata["conversation"] == conversation]
+        conv_ids = [
+            i
+            for i, metadata in enumerate(metadatas)
+            if metadata["conversation"] == conversation
+        ]
         # get indexes of documents that contain alias
         if name is not None:
             chunking_indexes = [i for i in conv_ids if name in documents[i]]
         else:
             # indexes should be spaced by overlap
-            chunking_indexes = conv_ids[::chunk_depth - overlap]
+            chunking_indexes = conv_ids[:: chunk_depth - overlap]
         # for each document at a chunking index, create a filtered document containting of the previous chunk_depth documents
         for i in chunking_indexes:
             if i - chunk_depth < 0:
@@ -117,12 +120,13 @@ def filter_and_chunk(documents, metadatas, chunk_depth, name=None, overlap=0):
 
 def format_blog(blog_documents, blog_metadatas, name, include_timestamp):
     formatted_blog = []
-    for doc, meta in tqdm(zip(blog_documents, blog_metadatas), total=len(blog_documents)):
-        meta['user_id'] = name
+    for doc, meta in tqdm(
+        zip(blog_documents, blog_metadatas), total=len(blog_documents)
+    ):
+        meta["user_id"] = name
         if include_timestamp:
-            if meta['timestamp'] is not None:
-                timestamp = pd.to_datetime(
-                    meta['timestamp']).strftime('%Y-%m-%d %H:%M')
+            if meta["timestamp"] is not None:
+                timestamp = pd.to_datetime(meta["timestamp"]).strftime("%Y-%m-%d %H:%M")
                 # check if timestamp is before 2000
                 if timestamp.split("-")[0] < "2000":
                     timestamp = f"Unknown Timestamp"
@@ -137,29 +141,34 @@ def format_blog(blog_documents, blog_metadatas, name, include_timestamp):
 
 def init_HuggingFaceEmbedding(index_info, name, include_timestamp):
     print("Initializing HuggingFaceEmbedding...")
-    model_name = index_info['model_name']
-    vector_dimension = index_info['vector_dimension']
-    index_name = index_info['index_name']
+    model_name = index_info["model_name"]
+    vector_dimension = index_info["vector_dimension"]
+    index_name = index_info["index_name"]
     rag_fname = f".llama_index/{model_name}/indexes/{index_name}"
-    embed_model = HuggingFaceEmbedding(
-        model_name=model_name)
+    embed_model = HuggingFaceEmbedding(model_name=model_name)
     Settings.embed_model = embed_model
     if not os.path.exists(rag_fname):
-        print(
-            f"Creating index at {rag_fname} from {index_info['document_path']}")
+        print(f"Creating index at {rag_fname} from {index_info['document_path']}")
         faiss_index = faiss.IndexFlatL2(vector_dimension)
-        if index_info['document_path'].endswith(".txt"):
-            documents = prep_txt_document(index_info['document_path'])
+        if index_info["document_path"].endswith(".txt"):
+            documents = prep_txt_document(index_info["document_path"])
         else:
             documents, metadata, blog_documents, blog_metadata = prep_parquet_documents(
-                index_info['document_path'], index_info['alias_dict'], include_timestamp=include_timestamp, name=name
+                index_info["document_path"],
+                index_info["alias_dict"],
+                include_timestamp=include_timestamp,
+                name=name,
             )
-            if index_info['overlap'] is None:
+            if index_info["overlap"] is None:
                 name = name
             else:
                 name = None
             chat_documents, chat_metadata = filter_and_chunk(
-                documents, metadata, index_info['chunk_depth'], name=name, overlap=index_info['overlap']
+                documents,
+                metadata,
+                index_info["chunk_depth"],
+                name=name,
+                overlap=index_info["overlap"],
             )
             documents = chat_documents + blog_documents
             metadata = chat_metadata + blog_metadata
@@ -171,17 +180,17 @@ def init_HuggingFaceEmbedding(index_info, name, include_timestamp):
                     metadata[i] = meta
                 for m in meta:
                     # if timestamp is not a string, convert to string
-                    if m['timestamp'] is not None and not isinstance(m['timestamp'], str):
-                        m['timestamp'] = m['timestamp'].strftime(
-                            '%Y-%m-%d %H:%M')
+                    if m["timestamp"] is not None and not isinstance(
+                        m["timestamp"], str
+                    ):
+                        m["timestamp"] = m["timestamp"].strftime("%Y-%m-%d %H:%M")
         documents = [Document(text=doc) for doc in documents]
         # assign metadata to documents
         for i, doc in enumerate(documents):
             doc.metadata = {"meta_list": metadata[i]}
             print("META: ", doc.metadata)
         vector_store = FaissVectorStore(faiss_index=faiss_index)
-        storage_context = StorageContext.from_defaults(
-            vector_store=vector_store)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
         print("Creating index...")
         index = VectorStoreIndex.from_documents(
             documents, storage_context=storage_context
@@ -201,24 +210,18 @@ class RAGModule:
         self.config = config
         print("CONFIG: ", self.config)
         self.k = k
-        self.index_info = config['index_info']
-        self.include_timestamp = config['include_timestamp']
+        self.index_info = config["index_info"]
+        self.include_timestamp = config["include_timestamp"]
         self.rag_index, self.rag_fname = init_HuggingFaceEmbedding(
-            self.index_info, config['name'], self.include_timestamp)
+            self.index_info, config["name"], self.include_timestamp
+        )
         self.rag_module = self.rag_index.as_retriever(similarity_top_k=k)
 
     def search(self, query):
-        if self.index_info['type'] == "RAGatouille":
-            retrieved = self.rag_module.search(query=query, k=self.k)
-            return [doc['content'] for doc in retrieved]
-        else:
-            retrieved = self.rag_module.retrieve(query)
-            return [doc.text for doc in retrieved]
+        retrieved = self.rag_module.retrieve(query)
+        return [doc.text for doc in retrieved]
 
     def update(self, document):
-        if self.index_info['type'] == "RAGatouille":
-            raise NotImplementedError("RAGatouille does not support updating")
-        else:
-            node = Document(text=document)
-            self.rag_index.insert(node)
-            self.rag_index.storage_context.persist(persist_dir=self.rag_fname)
+        node = Document(text=document)
+        self.rag_index.insert(node)
+        self.rag_index.storage_context.persist(persist_dir=self.rag_fname)
