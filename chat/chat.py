ecdf7517cd02442ac43d70519ff5d21db3705889
ksadov <ksadov@gmail.com>
2025-04-05 07:46:09 -0500

    get rid of local llm

--- chat/chat.py
+++ chat/chat.py
@@ -4,13 +4,14 @@ import torch
 import json
 from datetime import datetime as dt
 
-from chat.llm_inference import setup_llm
+from chat.llm_inference import LLM
 from chat.utils import HiddenPrints, parse_json
 from chat.conversation import ConvHistory, Message
 from chat.retrieval import RAGModule, RetrievalError
 
 # silence annoying ragatouille logging
 import logging
+
 logging.getLogger("ragatouille").setLevel(logging.CRITICAL)
 
 
@@ -20,32 +21,46 @@ class ChatController:
         self.config = json.load(open(bot_config_path))
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.rag_module = RAGModule(self.config, k)
-        self.llm = setup_llm(llm_config_path, self.device)
+        self.llm_config = json.load(open(llm_config_path))
+        self.llm = LLM(self.llm_config, self.device)
         with HiddenPrints():
             self.rag_module.search(query="warmup")
         self.conv_history = ConvHistory(
-            self.config["include_timestamp"], self.config["conversation_history_depth"], self.config["update_index_every"]
+            self.config["include_timestamp"],
+            self.config["conversation_history_depth"],
+            self.config["update_index_every"],
         )
 
     def make_response(self, query, speaker, conversation_name):
         query_timestamp = datetime.datetime.now()
         self.conv_history.add(
-            Message(conversation_name, query_timestamp, speaker, query))
-        if self.config['update_rag_index']:
+            Message(conversation_name, query_timestamp, speaker, query)
+        )
+        if self.config["update_rag_index"]:
             self.conv_history.update_rag_index(self.rag_module)
         try:
-            full_query = self.conv_history.str_of_depth(self.config['query_context_depth'])
+            full_query = self.conv_history.str_of_depth(
+                self.config["query_context_depth"]
+            )
             results = self.rag_module.search(query=full_query)
         except RetrievalError as e:
             results = []
             print(f"Error retrieving documents: {e}")
         prompt, response = self.llm.chat_step(
-            self.config['name'], speaker, self.config['description'], self.conv_history, results, self.config['include_timestamp']
+            self.config["name"],
+            speaker,
+            self.config["description"],
+            self.conv_history,
+            results,
+            self.config["include_timestamp"],
         )
         response_timestamp = datetime.datetime.now()
         self.conv_history.add(
-            Message(conversation_name, response_timestamp, self.config['name'], response))
-        if self.config['update_rag_index']:
+            Message(
+                conversation_name, response_timestamp, self.config["name"], response
+            )
+        )
+        if self.config["update_rag_index"]:
             self.conv_history.update_rag_index(self.rag_module)
         return prompt, response
 
@@ -56,9 +71,7 @@ def chat_loop(bot_config_path, llm_config_path, show_prompt, k):
         query = input("> ")
         if query == "exit":
             break
-        prompt, response = controller.make_response(
-            query, "user", "conversation_name"
-        )
+        prompt, response = controller.make_response(query, "user", "conversation_name")
         if show_prompt:
             print("------------------")
             print("PROMPT:")
@@ -69,13 +82,28 @@ def chat_loop(bot_config_path, llm_config_path, show_prompt, k):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--show_prompt", "-s",
-                        action="store_true", help="Print the system prompt")
-    parser.add_argument('--bot_config_path', '-b', type=str,
-                        help='Path to the config file', default='configs/bot/zef.json')
-    parser.add_argument('--llm_config_path', '-l', type=str,
-                        help='Path to the model config file', default='configs/llm/Mixtral-8x7B-v01.json')
-    parser.add_argument("--k", default=3, type=int,)
+    parser.add_argument(
+        "--show_prompt", "-s", action="store_true", help="Print the system prompt"
+    )
+    parser.add_argument(
+        "--bot_config_path",
+        "-b",
+        type=str,
+        help="Path to the config file",
+        default="configs/bot/zef.json",
+    )
+    parser.add_argument(
+        "--llm_config_path",
+        "-l",
+        type=str,
+        help="Path to the model config file",
+        default="configs/llm/Mixtral-8x7B-v01.json",
+    )
+    parser.add_argument(
+        "--k",
+        default=3,
+        type=int,
+    )
     args = parser.parse_args()
     chat_loop(args.bot_config_path, args.llm_config_path, args.show_prompt, args.k)
 
--- chat/llm_inference.py
+++ chat/llm_inference.py
@@ -1,111 +1,53 @@
+from typing import List, Tuple
 from transformers import pipeline, AutoTokenizer
 from datetime import datetime as dt
 import requests
 import json
 
-def make_context_string(rag_results):
+
+def make_context_string(rag_results: List[str]) -> str:
     context_string = ""
     for result in rag_results:
         context_string += f"\n\n- {result}"
     return context_string
 
 
-def make_instruct_query(name, description, conv_history, rag_results):
+def make_instruct_query(
+    name: str, description: str, conv_history: str, rag_results: List[str]
+) -> Tuple[str, str]:
     context_string = make_context_string(rag_results)
     system = f"You are playing the role of {name}, {description}."
-    user = f"You are chatting online. Write your next response in the conversation, based on the following excerpts" \
-           f"from {name}'s writing:{context_string}\n\nConversation history:\n\n{conv_history}\n{name}:"
+    user = (
+        f"You are chatting online. "
+        f"Write your next response in the conversation, based on the following excerpts"
+        f"from {name}'s writing:{context_string}\n\n"
+        f"Conversation history:\n\n{conv_history}\n{name}:"
+    )
     return system, user
 
 
-def make_completion_query(name, chat_user_name, description, conv_history, rag_results, include_timestamp):
+def make_completion_query(
+    name: str,
+    chat_user_name: str,
+    description: str,
+    conv_history: str,
+    rag_results: List[str],
+    include_timestamp: bool,
+) -> str:
     context_string = make_context_string(rag_results)
     if include_timestamp:
         timestamp_str = f"[{dt.now().strftime('%Y-%m-%d %H:%M')}] "
     else:
         timestamp_str = ""
-    prompt_str = f"Character sheet:\n\n{name}: {description}.\n\nExamples of {name}'s writing:{context_string}\n\n" \
-                 f"Conversation history:\n\n{conv_history}\n{timestamp_str}{name}:"
+    prompt_str = (
+        f"Character sheet:\n\n{name}: {description}.\n\n"
+        f"Examples of {name}'s writing:{context_string}\n\n"
+        f"Conversation history:\n\n{conv_history}\n{timestamp_str}{name}:"
+    )
     return prompt_str
 
-class LLM:
-    def __init__(self, config, device):
-        self.config = config
-        self.model_name = self.config["model"]
-        self.device = device
-        self.instruct = self.config["instruct"]
 
-    def chat_step(self, name, chat_user_name, description, conv_history, rag_results, include_timestamp):
-        if self.instruct:
-            system, user = make_instruct_query(name, description, conv_history, rag_results)
-            prompt = {"system": system, "user": user}
-            response = self.make_instruct_request(system, user)
-        else:
-            prompt = make_completion_query(name, chat_user_name, description, conv_history, rag_results, include_timestamp)
-            response = self.make_completion_request(prompt, name, chat_user_name)
-        return prompt, response
-
-
-    def make_instruct_request(self, prompt):
-        pass
-
-    def make_completion_request(self, prompt, name, chat_user_name):
-        pass
-
-class LocalLLM(LLM):
-    def __init__(self, config, device):
-        super().__init__(config, device)
-        self.pipe = pipeline("text-generation", model=self.model_name, device=device, trust_remote_code=True, torch_dtype='float16')
-        self.instruct = config["instruct"]
-
-
-    def make_instruct_request(self, system, user):
-        messages = [{"role": "system", "content": system}, {"role": "user", "content": user}]
-        output = self.pipe(messages)
-        return cleanup_output(output, system, user)
-
-
-    def make_completion_request(self, prompt, target_name, chat_user_name):
-        output = self.pipe(prompt)[0]['generated_text']
-        return cleanup_output(output, prompt, target_name, chat_user_name)
-
-class RemoteLLM(LLM):
-    def __init__(self, config, device):
-        super().__init__(config, device)
-        self.api_base = self.config["api_base"]
-        self.api_key = self.config["api_key"]
-        self.prompt_params = self.config["prompt_params"]
-        self.model = self.config["model"]
-
-    def make_instruct_request(self, system, user):
-        # use chat completion api
-        response = requests.post(
-            self.api_base,
-            headers={"Authorization": f"Bearer {self.api_key}"},
-            json={"model": self.model, "messages": [{"role": "system", "content": system}, {"role": "user", "content": user}], **self.prompt_params},
-        )
-        return response.json()["choices"][0]["message"]["content"]
-
-    def make_completion_request(self, prompt, name, chat_user_name):
-        # use completion api
-        response = requests.post(
-            self.api_base,
-            headers={"Authorization": f"Bearer {self.api_key}"},
-            json={"model": self.model, "prompt": prompt, **self.prompt_params},
-        )
-        raw_response = response.json()["choices"][0]["text"]
-        return cleanup_output(raw_response, name, chat_user_name)
-
-
-def setup_llm(config_path, device):
-    config = json.load(open(config_path))
-    if config["api_base"] is not None:
-        return RemoteLLM(config, device)
-    else:
-        return LocalLLM(config, device)
-
-
-def cleanup_output(output, target_name, chat_user_name):
+def cleanup_output(output: str, target_name: str, chat_user_name: str) -> str:
     # trim everything after the first instance of responder name, if there is one
     output_trimmed = output.split(chat_user_name)[0]
     # trim whitespace in front and back
@@ -115,14 +57,81 @@ def cleanup_output(output, target_name, chat_user_name):
     # trim before first :, if there is one
     colon = output_trimmed.find(f"{target_name}:") + len(target_name) + 1
     if colon != -1:
-        output_trimmed = output_trimmed[colon+1:]
+        output_trimmed = output_trimmed[colon + 1 :]
     # trim anything after the last period, unless there are no periods
     periods = [i for i, c in enumerate(output_trimmed) if c == "."]
     if len(periods) > 0:
-        output_trimmed = output_trimmed[:periods[-1]+1]
+        output_trimmed = output_trimmed[: periods[-1] + 1]
     # get rid of <s> and </s>
     output_trimmed = output_trimmed.replace("<s>", "")
     output_trimmed = output_trimmed.replace("</s>", "")
     # trim whitespace again
     output_trimmed = output_trimmed.strip()
     return output_trimmed
+
+
+class LLM:
+    def __init__(self, config: dict, device: str):
+        self.config = config
+        self.model_name = self.config["model"]
+        self.device = device
+        self.instruct = self.config["instruct"]
+        self.api_base = self.config["api_base"]
+        self.api_key = self.config["api_key"]
+        self.prompt_params = self.config["prompt_params"]
+        self.model = self.config["model"]
+
+    def chat_step(
+        self,
+        name: str,
+        chat_user_name: str,
+        description: str,
+        conv_history: str,
+        rag_results: List[str],
+        include_timestamp: bool,
+    ) -> Tuple[str, str]:
+        if self.instruct:
+            system, user = make_instruct_query(
+                name, description, conv_history, rag_results
+            )
+            prompt = {"system": system, "user": user}
+            response = self.make_instruct_request(system, user)
+        else:
+            prompt = make_completion_query(
+                name,
+                chat_user_name,
+                description,
+                conv_history,
+                rag_results,
+                include_timestamp,
+            )
+            response = self.make_completion_request(prompt, name, chat_user_name)
+        return prompt, response
+
+    def make_instruct_request(self, system: str, user: str) -> str:
+        # use chat completion api: https://platform.openai.com/docs/api-reference/chat
+        response = requests.post(
+            self.api_base,
+            headers={"Authorization": f"Bearer {self.api_key}"},
+            json={
+                "model": self.model,
+                "messages": [
+                    {"role": "system", "content": system},
+                    {"role": "user", "content": user},
+                ],
+                **self.prompt_params,
+            },
+        )
+        return response.json()["choices"][0]["message"]["content"]
+
+    def make_completion_request(
+        self, prompt: str, name: str, chat_user_name: str
+    ) -> str:
+        # use completion api: https://platform.openai.com/docs/api-reference/completions
+        response = requests.post(
+            self.api_base,
+            headers={"Authorization": f"Bearer {self.api_key}"},
+            json={"model": self.model, "prompt": prompt, **self.prompt_params},
+        )
+        raw_response = response.json()["choices"][0]["text"]
+        return cleanup_output(raw_response, name, chat_user_name)
