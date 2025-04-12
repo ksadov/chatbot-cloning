cad6e3a08ca317ab9ccfa8bf426e1131c6e684e1
ksadov <ksadov@gmail.com>
2025-04-12 12:20:35 -0500

    get rid of pandas and pyarrow stuff

--- src/bot/message.py
+++ src/bot/message.py
@@ -1,8 +1,5 @@
 import datetime
 
-import pandas as pd
-import pyarrow as pa
-
 
 class Message:
     def __init__(
@@ -16,22 +13,6 @@ class Message:
         self.timestamp = timestamp
         self.user_id = user_id
         self.content = content
-
-    def to_pandas(self):
-        dframe_dict = {
-            "conversation": self.conversation,
-            "user_id": self.user_id,
-            "timestamp": self.timestamp,
-            "content": self.content,
-        }
-        dataframe = pd.DataFrame([dframe_dict])
-        return dataframe
-
-    def to_arrow(self):
-        try:
-            return pa.RecordBatch.from_pandas(self.to_pandas())
-        except:
-            raise
 
     def __str__(self):
         return f"Message(conversation={self.conversation}, user_id={self.user_id}, timestamp={self.timestamp.strftime('%Y-%m-%d %H:%M')}, content={self.content})"
