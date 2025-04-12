e55ae4f61004f5556ca48bd8243a7cb383687398
ksadov <ksadov@gmail.com>
2025-04-12 15:22:29 -0500

    make discord messages work with new chat loop format

--- src/bot/discord_bot.py
+++ src/bot/discord_bot.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import discord
 
 from src.bot.chat_controller import ChatController
+from src.bot.message import Message
 from src.utils.local_logger import LocalLogger
 
 intents = discord.Intents.default()
@@ -26,6 +27,26 @@ class DiscordBot(discord.Client):
     async def on_ready(self):
         self.logger.info(f"{self.user} has connected to Discord!")
 
+    def message_from_discord_message(self, message: discord.Message) -> Message:
+        return Message(
+            conversation=message.channel.id,
+            platform="discord",
+            sender_name=message.author.name,
+            text_content=message.content,
+            timestamp=message.created_at,
+            bot_config=self.discord_config,
+            platform_specific_message_id=message.id,
+            platform_specific_user_id=message.author.id,
+            global_user_id=message.author.id,
+            attachments=message.attachments,
+            replies_to_message_id=(
+                message.reference.message_id if message.reference else None
+            ),
+            reactions=message.reactions,
+            server_nickname=message.author.nick,
+            account_username=message.author.name,
+        )
+
     def can_answer(self, message: discord.Message) -> bool:
         is_dm = isinstance(message.channel, discord.DMChannel)
         if message.author == self.user:
@@ -37,26 +58,29 @@ class DiscordBot(discord.Client):
             return False
 
     async def on_message(self, message: discord.Message):
-        if self.can_answer(message):
-            conversation_name = message.channel.id
+        if message.author == self.user:
+            self_message = self.message_from_discord_message(message)
+            self.chat_controller.update_conv_history(self_message)
+            return
+        elif self.can_answer(message):
+            user_message = self.message_from_discord_message(message)
+            self.chat_controller.update_conv_history(user_message)
             self.logger.info(f"Received message: {message.content}")
             if message.content == self.discord_config["clear_command"]:
                 self.conv_history.clear()
                 self.logger.info(
-                    f"Conversation history cleared for channel {conversation_name}"
+                    f"Conversation history cleared for channel {message.channel.id}"
                 )
                 await message.channel.send("[Conversation history cleared]")
             else:
-                prompt, responses = self.chat_controller.make_response(
-                    message.content, message.author.name, conversation_name
-                )
+                prompt, responses = self.chat_controller.make_response(user_message)
                 self.logger.debug(f"Prompt: {prompt}")
                 self.logger.debug(f"Responses: {responses}")
                 # only send next response once previous response is sent
                 for response in responses:
                     await message.channel.send(response)
                     await asyncio.sleep(1)
-                self.logger.debug(f"Sent response to channel {conversation_name}")
+                self.logger.debug(f"Sent response to channel {message.channel.id}")
 
     async def on_error(self, event_method, *args):
         self.logger.error(f"Error in event {event_method}: {args}")
