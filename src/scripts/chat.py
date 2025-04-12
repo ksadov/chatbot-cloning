00cc9ee7caade4872d2d3ddccc95c790abf3431d
ksadov <ksadov@gmail.com>
2025-04-12 16:15:17 -0500

    fix chat to work with new message saving

--- src/scripts/chat.py
+++ src/scripts/chat.py
@@ -2,18 +2,26 @@ import argparse
 import datetime
 import json
 from pathlib import Path
+from typing import Optional
 
 from src.bot.chat_controller import ChatController
 from src.bot.message import Message
+from src.message_database.utils import database_from_config_path
 from src.utils.local_logger import LocalLogger
 
 
 def chat_loop(
     bot_config_path: Path,
+    database_config_path: Optional[Path],
     show_prompt: bool,
     logger: LocalLogger,
 ):
     controller = ChatController(bot_config_path, logger)
+    database = (
+        database_from_config_path(database_config_path)
+        if database_config_path
+        else None
+    )
     with open(bot_config_path, "r") as f:
         config = json.load(f)
     while True:
@@ -29,6 +37,8 @@ def chat_loop(
             timestamp=datetime.datetime.now(),
             bot_config=config,
         )
+        if database:
+            database.store_message(message)
         controller.update_conv_history(message)
         prompt, responses = controller.make_response(message)
         if show_prompt:
@@ -45,6 +55,8 @@ def chat_loop(
                 timestamp=datetime.datetime.now(),
                 bot_config=config,
             )
+            if database:
+                database.store_message(message)
             print(response)
             controller.update_conv_history(message)
 
@@ -60,6 +72,13 @@ def main():
         type=Path,
         help="Path to the config file",
         default="configs/bot/zef_completion.json",
+    )
+    parser.add_argument(
+        "--database_config_path",
+        "-db",
+        type=Path,
+        help="Path to the database config file",
+        default=None,
     )
     parser.add_argument(
         "--log_dir",
@@ -86,7 +105,12 @@ def main():
     logger = LocalLogger(
         args.log_dir, "chat", args.console_log_level, args.file_log_level
     )
-    chat_loop(args.bot_config_path, args.show_prompt, logger)
+    chat_loop(
+        args.bot_config_path,
+        args.database_config_path,
+        args.show_prompt,
+        logger,
+    )
 
 
 if __name__ == "__main__":
